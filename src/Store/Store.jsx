fbbbede5fa410a2ca46dc9f50118f2ba4d8fa592
Ayush Bahuguna <ayushbahuguna006@gmail.com>
2026-01-13 12:59:15 +0530

    Update Store.jsx

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,8 +1,8 @@
 
 // Creating a store For State management Using zustand
    Storing Information further used in other components anytime information wants to be consumed by any other component
-   Fetching information by the components without props drilling .direct one to one interaction between parents and child component between which information is being shared
-
+   Fetching information by the components without props drilling direct one to one interaction between parents and child component between which information is being shared
+   
 
 
 import { create } from 'zustand'
