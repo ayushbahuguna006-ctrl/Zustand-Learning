4842c1b142f7e55e41fd7e2d2aa71612aea794e3
Ayush Bahuguna <ayushbahuguna006@gmail.com>
2026-01-13 13:00:21 +0530

    Update Store.jsx

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -2,7 +2,7 @@
 // Creating a store For State management Using zustand
    Storing Information further used in other components anytime information wants to be consumed by any other component
    Fetching information by the components without props drilling direct one to one interaction between parents and child component between which information is being shared
-   
+   No use of Boiler plates as in Redux
 
 
 import { create } from 'zustand'
